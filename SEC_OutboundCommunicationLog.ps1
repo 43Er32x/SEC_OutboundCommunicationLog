25939faf624b520d16d4bacfeba2b3fe01c8cb2f
43Er32x <160287229+43Er32x@users.noreply.github.com>
2024-02-18 19:28:37 +0100

    Update SEC_OutboundCommunicationLog.ps1
    Exclude IPV6 API VirusTotal request

--- SEC_OutboundCommunicationLog.ps1
+++ SEC_OutboundCommunicationLog.ps1
@@ -7,14 +7,14 @@ if (-not (Test-Path -Path (Split-Path $LogFile))) {
 }
 
 # Définition de l'en-tête CSV
-$Header = "Timestamp,ProcessName,ProcessId,ProcessPath,LocalAddress,LocalPort,RemoteAddress,RemotePort,Protocol,VirusTotalHarmless,VirusTotalMalicious,VirusTotalSuspicious,VirusTotalUndetected,VirusTotalTimeout "
+$Header = "Timestamp,ProcessName,ProcessId,ProcessPath,LocalAddress,LocalPort,RemoteAddress,RemotePort,Protocol,VirusTotalHarmless,VirusTotalMalicious,VirusTotalSuspicious,VirusTotalUndetected,VirusTotalTimeout"
 
 # Écriture de l'en-tête dans le fichier de log
 Add-content -Path $LogFile -Value $Header
 
 # Fonction pour obtenir le rapport VirusTotal pour une adresse IP
 function GetVirusTotalReport($IPAddress) {
-    $APIKey = "YOUR API KEY"
+    $APIKey = "API KEY"
     $url = "https://www.virustotal.com/api/v3/ip_addresses/$IPAddress"
     $headers = @{
         "x-apikey" = $APIKey
@@ -45,15 +45,34 @@ while ($true) {
         # Obtention du nom du protocole
         $Protocol = GetProtocolName $ProtocolNumber
 
-        # Analyse de l'adresse IP de destination avec VirusTotal
-        $VirusTotalHarmless = (GetVirusTotalReport $RemoteAddress).data.attributes.last_analysis_stats.harmless
-        $VirusTotalMalicious = (GetVirusTotalReport $RemoteAddress).data.attributes.last_analysis_stats.malicious
-        $VirusTotalSuspicious = (GetVirusTotalReport $RemoteAddress).data.attributes.last_analysis_stats.suspicious
-        $VirusTotalUndetected = (GetVirusTotalReport $RemoteAddress).data.attributes.last_analysis_stats.undetected
-        $VirusTotalTimeout = (GetVirusTotalReport $RemoteAddress).data.attributes.last_analysis_stats.timeout
-
         # Construction du message à logger
-        $LogMessage = "$((Get-Date).ToString('MM/dd/yyyy HH:mm:ss')), $($Process.Name), $ProcessId, $ProcessPath, $LocalAddress, $LocalPort, $RemoteAddress, $RemotePort, $Protocol, $VirusTotalHarmless, $VirusTotalMalicious, $VirusTotalSuspicious, $VirusTotalUndetected, $VirusTotalTimeout"
+        $LogMessage = "$((Get-Date).ToString('MM/dd/yyyy HH:mm:ss')), $($Process.Name), $ProcessId, $ProcessPath, $LocalAddress, $LocalPort, $RemoteAddress, $RemotePort, $Protocol"
+
+        # Si l'adresse est IPv4, on fait l'analyse VirusTotal
+        if ($LocalAddress -match '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b') {
+            $VirusTotalReport = GetVirusTotalReport $RemoteAddress
+
+            if ($VirusTotalReport.data) {
+                $VirusTotalHarmless = $VirusTotalReport.data.attributes.last_analysis_stats.harmless
+                $VirusTotalMalicious = $VirusTotalReport.data.attributes.last_analysis_stats.malicious
+                $VirusTotalSuspicious = $VirusTotalReport.data.attributes.last_analysis_stats.suspicious
+                $VirusTotalUndetected = $VirusTotalReport.data.attributes.last_analysis_stats.undetected
+                $VirusTotalTimeout = $VirusTotalReport.data.attributes.last_analysis_stats.timeout
+            } else {
+                # Si aucune donnée n'est disponible, mettre "N/A"
+                $VirusTotalHarmless = "N/A"
+                $VirusTotalMalicious = "N/A"
+                $VirusTotalSuspicious = "N/A"
+                $VirusTotalUndetected = "N/A"
+                $VirusTotalTimeout = "N/A"
+            }
+
+            # Ajout des données VirusTotal dans le message
+            $LogMessage += ", $VirusTotalHarmless, $VirusTotalMalicious, $VirusTotalSuspicious, $VirusTotalUndetected, $VirusTotalTimeout"
+        } else {
+            # Pour les adresses IPv6, mettre "N/A" pour les données VirusTotal
+            $LogMessage += ", N/A, N/A, N/A, N/A, N/A"
+        }
 
         # Écriture du message dans le fichier de log
         Add-content -Path $LogFile -Value $LogMessage
